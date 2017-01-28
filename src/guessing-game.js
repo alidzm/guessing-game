b5b247df5f2ed54cd52eddd91115ed23dcc564c2
alidzm <qarz@tut.by>
2017-01-29 01:24:28 +0300

    guessing-game: final commit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,22 +1,26 @@
 class GuessingGame {
     constructor() {
-
+    	this.result = 0;
+    	this.min = 0;
+    	this.max = 0;
     }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	this.result = Math.ceil(this.min + (this.max - this.min) / 2);
+    	return this.result;
     }
 
     lower() {
-
+    	this.setRange(this.min, this.result);
     }
 
     greater() {
-
+    	this.setRange(this.result, this.max);
     }
 }
 
